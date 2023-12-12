8d5ee6f583c7a4200829c2caf08b665a234375c2
Protyasha-Roy <protyasharoy369@gmail.com>
2023-12-13 00:37:18 +0600

    changed the port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(cors());
